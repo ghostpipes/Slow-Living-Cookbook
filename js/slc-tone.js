5c765ea5c1bf4548372eb33591836862296432fc
ashley <omgitsashleyroland@gmail.com>
2022-03-25 12:56:49 -0700

    Restructured JS - global functions for global volume

---
+++ js/slc-tone.js
@@ -0,0 +1,68 @@
+var delta = 0;
+var synthIntervalA;
+var synthIntervalB;
+var synthIntervalC;
+var synthIntervalD;
+
+function loop() {
+    const loop = setInterval(anim, 10);
+}
+
+function anim() {
+    delta += 0.01;
+    setRotation(delta);
+    // console.log(delta);
+    delta %= 6;
+}
+
+function createDronePlusPanner(positionX, positionY, positionZ) {
+    const panner = new Tone.Panner3D({
+        panningModel: "HRTF",
+        positionX,
+        positionY,
+        positionZ,
+    }).toDestination();
+
+    const synth = new Tone.Synth({
+        oscillator: {
+            type: "sine"
+        },
+        envelope: {
+            attack: 0.95,
+            decay: 0.95,
+            sustain: .95,
+            release: 5
+        },
+        volume: -12
+    }).connect(panner);
+    
+    return synth;
+}
+
+
+function chord(listOfFreqs) {
+
+    for (const freq of listOfFreqs) {
+        monoDrone(freq);
+    }
+
+}
+
+
+function monoDrone(val) {
+    var randX = Math.random() * 2;
+    var randY = Math.random() * 2;
+    var randZ = Math.random() * 2;
+
+    var synth = createDronePlusPanner(randX,randY,randZ);
+    const now = Tone.now();
+
+    synth.triggerAttack(val, now);
+    synth.triggerRelease(now+28);
+    }
+
+function setRotation(angle) {
+    Tone.Listener.forwardX.value = Math.sin(angle);
+    Tone.Listener.forwardY.value = 0;
+    Tone.Listener.forwardZ.value = -Math.cos(angle);
+}
